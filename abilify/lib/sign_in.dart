1e72ffc5d3c0e151c7c9e38a1d6d4e97e114c3ad
Harshs-06 <harsh.soni.23cse@bmu.edu.in>
2025-04-17 23:23:22 +0530

    sign_in

--- abilify/lib/sign_in.dart
+++ abilify/lib/sign_in.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SignIn extends StatefulWidget {
   @override
@@ -12,12 +13,11 @@ class _SignInState extends State<SignIn> {
   bool obscureText = true;
 
   void _togglePasswordVisibility() {
-    if(passwordController.text.isNotEmpty){
-         setState(() {
-      obscureText = !obscureText;
-    });
+    if (passwordController.text.isNotEmpty) {
+      setState(() {
+        obscureText = !obscureText;
+      });
     }
-    
   }
 
   void _checkInput() {
@@ -53,51 +53,104 @@ class _SignInState extends State<SignIn> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Container(
+                width: 300,
+    
                 decoration: BoxDecoration(
                   color: Colors.white,
-                  borderRadius: BorderRadius.circular(12),
-                  boxShadow: [BoxShadow(
-                    color: Colors.black12,
-                    blurRadius:4,
-                    offset: Offset(0, 2), 
-                  )]
+                  borderRadius: BorderRadius.circular(7),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black12,
+                      blurRadius: 4,
+                      offset: Offset(0, 2),
+                    ),
+                  ],
                 ),
                 child: TextField(
                   keyboardType: TextInputType.emailAddress,
                   controller: emailController,
-                  decoration: InputDecoration(labelText: 'Email',labelStyle: TextStyle(color: Colors.black,fontWeight: FontWeight.w600),border: OutlineInputBorder(borderRadius: BorderRadius.circular(12),borderSide: BorderSide.none),
-                  filled: true,
-                  fillColor: Colors.white,
-                  prefixIcon: Icon(Icons.person)
+                  decoration: InputDecoration(
+                    labelText: 'UserName',
+                    labelStyle: GoogleFonts.poppins(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w600,
+                      
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(7),
+                      borderSide: BorderSide.none,
+                    ),
+                    filled: true,
+                    fillColor: Colors.white,
+                    prefixIcon: Icon(Icons.person),
                   ),
                 ),
               ),
               SizedBox(height: 12),
-              Container(decoration: BoxDecoration(
+              Container(
+                decoration: BoxDecoration(
                   color: Colors.white,
-                  borderRadius: BorderRadius.circular(12),
-                  boxShadow: [BoxShadow(
-                    color: Colors.black12,
-                    blurRadius:4,
-                    offset: Offset(0, 2), 
-                  )]
+                  borderRadius: BorderRadius.circular(7),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black12,
+                      blurRadius: 4,
+                      offset: Offset(0, 2),
+                    ),
+                  ],
                 ),
                 child: TextField(
                   keyboardType: TextInputType.visiblePassword,
                   controller: passwordController,
-                  decoration: InputDecoration(labelText: 'Password',labelStyle: TextStyle(color: Colors.black,fontWeight: FontWeight.w600),border: OutlineInputBorder(borderRadius: BorderRadius.circular(12),borderSide: BorderSide.none),
-                  suffixIcon: IconButton(onPressed: _togglePasswordVisibility, icon: Icon(
-                    obscureText?Icons.visibility:Icons.visibility_off)),
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    
+                    labelStyle: GoogleFonts.poppins(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w600,
+                      
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(7),
+                      borderSide: BorderSide.none,
+                    ),
+                    suffixIcon: IconButton(
+                      onPressed: _togglePasswordVisibility,
+                      icon: Icon(
+                        obscureText ? Icons.visibility : Icons.visibility_off,
+                      ),
+                    ),
                     filled: true,
-                  fillColor: Colors.white,
-                    prefixIcon: Icon(Icons.lock)
+                    fillColor: Colors.white,
+                    prefixIcon: Icon(Icons.lock),
                   ),
                   obscureText: obscureText,
                 ),
               ),
-              ElevatedButton(onPressed: (){}, child: Center(
-                child: Text("Sign In"),
-              ))
+              SizedBox(height: 12),
+              ElevatedButton(
+                onPressed: isButtonEnabled ? () {} : null,
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size(300, 50),
+                  maximumSize: Size(300, 50),
+                  backgroundColor:
+                      isButtonEnabled
+                          ? Color.fromARGB(255, 255, 125, 89)
+                          : Color.fromARGB(255, 255, 185, 166),
+                  disabledBackgroundColor: Color.fromARGB(255, 255, 185, 166),
+                  foregroundColor: Colors.white,
+                  disabledForegroundColor: Colors.white70,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(7),
+                  ),
+                ),
+                child:Center(child: Text(
+                  "Sign In",
+                  style: GoogleFonts.poppins(fontSize: 20,fontWeight: FontWeight.w700),
+                  ),
+                
+                ),
+              ),
             ],
           ),
         ),
